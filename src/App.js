a0b49430586cbcc6a903e49ba1fab1aa4ab0d598
ofek shtrit <ofek1997shtrit@gmail.com>
2022-08-25 13:17:54 +0300

    added react route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
+
 
 function App() {
   return (
@@ -16,10 +18,48 @@ function App() {
           rel="noopener noreferrer"
         >
           <button>press me</button>
-          
+
         </a>
       </header>
+      <div className="RouteTest">
+        <h1>Welcome to React Router Test!</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </div>
     </div>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          That feels like an existential question, don't you
+          think?
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
   );
 }
 
