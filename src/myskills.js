5c0da8789913430eee6089ddeb777f37f406e78f
ofek shtrit <ofek1997shtrit@gmail.com>
2022-09-03 04:24:26 +0300

    improved structure of project page component

--- src/myskills.js
+++ src/myskills.js
@@ -1,42 +1,86 @@
 import { Icons, Skills } from './skills'
 import './myskills.css'
 import profilePic from './images/ProfilePicture.png'
+import ScrollTrigger from 'react-scroll-trigger';
+import React from 'react'
 
-export function MySkills() {
-  return (
-    <div>
-      <h1>My skills</h1>
-      <h2>About my self</h2>
-      <p>My name is Ofek, I'm 25 Y.O from Tel-Aviv israel,
-        I would say I'm pretty outgoing and social, I love to spend time hanging with
-        friends & familiy.
-        I keep an active lifestyle with a variety of diffrent sports, I'll surf, play football, soccer  & workout!
-        Every once in a while, I'd pack a bag, and go on a trip, by foot or my motorsycle.
-      </p>
-      <p><strong>Keep reading my bio</strong></p>
-      < Skills />
-    </div>
-  )
+export class MySkills extends React.Component {
+
+  onEnterViewport = () => {
+    setTimeout( () => {
+      this.setState({
+        visible: true,
+      })
+    }, 800);
+  }
+
+  onExitViewport = () => {
+    this.setState({
+      visible: false,
+    });
+  }
+
+  render() {
+    return (
+    < ScrollTrigger>
+      <div>
+        <h1>My skills</h1>
+        <h2>About my self</h2>
+        <p>My name is Ofek, I'm 25 Y.O from Tel-Aviv israel,
+          I would say I'm pretty outgoing and social, I love to spend time hanging with
+          friends & familiy.
+          I keep an active lifestyle with a variety of diffrent sports, I'll surf, play football, soccer  & workout!
+          Every once in a while, I'd pack a bag, and go on a trip, by foot or my motorsycle.
+        </p>
+        <p><strong>Keep reading my bio</strong></p>
+        < Skills />
+      </div>
+    </ScrollTrigger>
+    )
+  }
 }
 
-export function MySkillsTest() {
-  return (
-      <div className='content-card'>
-        <div className='info'>
-          <img src={profilePic} alt='my profile img' />
-          <div className='profile-info'>
-            <h1>My skills</h1>
-            <h3>About my self</h3>
-            <p>My name is Ofek, I'm 25 Y.O from Tel-Aviv israel,
-               I would say I'm pretty outgoing and social, I love to spend time hanging with
-               friends & familiy.
-               I keep an active lifestyle with a variety of diffrent sports, I'll surf, play football, soccer  & workout!
-               Every once in a while, I'd pack a bag, and go on a trip, by foot or my motorsycle.</p>
+export class MySkillsTest extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {visible: ''};
+  }
+
+  onEnterViewport = () => {
+    setTimeout( () => {
+      this.setState({
+        visible: true,
+      })
+    }, 800);
+  }
+
+  onExitViewport = () => {
+    this.setState({
+      visible: false,
+    });
+  }
+
+  render() {
+    return (
+      < ScrollTrigger onEnter={this.onEnterViewport} onExit={this.onExitViewport}>
+        <div className={`content-card container ${this.state.visible ? 'personal-animate' : ''}`}>
+          <div className='info'>
+            <img src={profilePic} alt='my profile img' />
+            <div className='profile-info'>
+              <h1>My skills</h1>
+              <h3>About my self</h3>
+              <p>My name is Ofek, I'm 25 Y.O from Tel-Aviv israel,
+                I would say I'm pretty outgoing and social, I love to spend time hanging with
+                friends & familiy.
+                I keep an active lifestyle with a variety of diffrent sports, I'll surf, play football, soccer  & workout!
+                Every once in a while, I'd pack a bag, and go on a trip, by foot or my motorsycle.</p>
+            </div>
+          </div>
+          <div className='badges'>
+            < Icons />
           </div>
         </div>
-        <div className='badges'>
-          < Icons />
-        </div>
-      </div>
-  )
+      </ ScrollTrigger>
+    )
+  }
 }
--- src/projectPages/projectPage.js
+++ src/projectPages/projectPage.js
@@ -8,19 +8,23 @@ export class ProjectPageStructureText extends React.Component {
   }
   render() {
     return (
-      <div>
+      <>
         < NavBar />
-        <h1>{this.props.projectName}</h1>
-        <h2>Project Purpose and Goal</h2>
-        <p>{this.props.projectPurpose}</p>
-        <h2>Web Stack and Explanation</h2>
-        <p>{this.props.projectWebStack}</p>
-        <h2>Problems and Thought Process</h2>
-        <p>{this.props.projectProblens}</p>
-        <h2>Lessons Learned</h2>
-        <p>{this.props.projectlessons}</p>
+        <div className='project-page-container'>
+          <div className='project-page-content'>
+            <h1>{this.props.projectName}</h1>
+            <h2>Project Purpose and Goal</h2>
+            <p>{this.props.projectPurpose}</p>
+            <h2>Web Stack and Explanation</h2>
+            <p>{this.props.projectWebStack}</p>
+            <h2>Problems and Thought Process</h2>
+            <p>{this.props.projectProblens}</p>
+            <h2>Lessons Learned</h2>
+            <p>{this.props.projectlessons}</p>
+          </div>
+        </div>
         < Footer />
-      </div>
+      </>
     )
   }
 }
