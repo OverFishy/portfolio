a22f598bcfa20a6c3a16561ab2423ab234e2eab9
ofek shtrit <ofek1997shtrit@gmail.com>
2022-09-06 01:35:28 +0300

    changed the linkes to redirect you to a new tab rather then redirect your current one, I think Lauri have passed through the about page, I dont know about the rest

--- src/myskills.js
+++ src/myskills.js
@@ -69,12 +69,12 @@ export class MySkillsTest extends React.Component {
             <img src={profilePic} alt='my profile img' />
             <div className='profile-info'>
               <h1>My skills</h1>
-              <h3>About my self</h3>
-              <p>My name is Ofek, I'm 25 Y.O from Tel-Aviv israel,
-                I would say I'm pretty outgoing and social, I love to spend time hanging with
-                friends & familiy.
-                I keep an active lifestyle with a variety of diffrent sports, I'll surf, play football, soccer  & workout!
-                Every once in a while, I'd pack a bag, and go on a trip, by foot or my motorsycle.</p>
+              <h3>About myself</h3>
+              <p>My name is Ofek, I'm 25 Y.O from Tel-Aviv Israel,
+                I would say I'm pretty outgoing and social and love to spend time hanging with
+                friends & family.
+                I keep an active lifestyle with a variety of different sports, I surf, play football, soccer  & workout!
+                Every once in a while, I pack a bag, and go on a trip, by foot or my motorcycle.</p>
                 <Link to="/about">Keep reading my bio</Link>
             </div>
           </div>
--- src/navbar.js
+++ src/navbar.js
@@ -19,7 +19,7 @@ export function NavBar() {
     <nav>
       <ul className="menuItems">
         <li><Link to="/" data-item='Home'>Home</Link></li>
-        <li><Link to="/#projects" data-item='Projects'>projects</Link></li>
+        <li><Link to="/#projects" data-item='Projects'>Projects</Link></li>
         <li><Link to="/about" data-item='About'>About</Link></li>
         <li><Link to="/#contact" data-item='Contact'>Contact</Link></li>
         {/* <li><a href='/' data-item='Home'>Home</a></li>
